8547785ec04af20e6557729b1bf33d065f5c49a2
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-02 01:04:52 +0600

    admin division route is added

---
+++ api/routes/admin/division.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const checkAdminAuth = require('../../middleware/adminAuth');
+const divisionController = require('../../controller/admin/division');
+
+router.get('/all', divisionController.getAllDivision);
+router.post('/create', divisionController.createDivision);
+router.patch('/update/:divisionID', divisionController.updateDivision);
+router.delete('/delete/:divisionID', divisionController.deteleteDivision);
+
+module.exports = router;
