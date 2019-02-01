ca9f82ed000c698b86875b388317555434e25bc8
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-02 00:56:58 +0600

    cors and mongoose is added

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
+
+mongoose.connect('mongodb://localhost:27017/PersonalBlog', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     const error = new Error("Not found");
