4d47d540135de76b63dbd58a313c53d06d542d7a
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-06 17:39:42 +0600

    minArea route is updated

--- api/routes/admin/minArea.js
+++ api/routes/admin/minArea.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const checkAdminAuth = require('../../middleware/adminAuth');
 const minAreaController = require('../../controller/admin/minArea');
 
-router.get('/all', minAreaController.getAllMaxArea);
-router.post('/create', minAreaController.createMaxArea);
-router.patch('/update/:minAreaID', minAreaController.updateMaxArea);
-router.delete('/delete/:minAreaID', minAreaController.deteleteMaxArea);
+router.get('/all', minAreaController.getAllMinArea);
+router.post('/create', minAreaController.createMinArea);
+router.patch('/update/:minAreaID', minAreaController.updateMinArea);
+router.delete('/delete/:minAreaID', minAreaController.deleteMinArea);
 
 module.exports = router;
