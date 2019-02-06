40a39dc0fe3b5858f01ad619194874bcdf609e1a
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-06 17:40:20 +0600

    room route is updated

--- api/routes/admin/room.js
+++ api/routes/admin/room.js
@@ -6,6 +6,6 @@ const roomController = require('../../controller/admin/room');
 router.get('/all', roomController.getAllRoom);
 router.post('/create', roomController.createRoom);
 router.patch('/update/:roomID', roomController.updateRoom);
-router.delete('/delete/:roomID', roomController.deteleteRoom);
+router.delete('/delete/:roomID', roomController.deleteRoom);
 
 module.exports = router;
