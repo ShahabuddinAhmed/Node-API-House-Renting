7a229e7da016bb88d55eb3be0386e282fe45e7ce
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-06 17:40:44 +0600

    app file is updated

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors');
 
+const userRoutes = require('./api/routes/user/user');
+const houseAdsRoutes = require('./api/routes/user/houseAds');
+const coverImageRoutes = require('./api/routes/user/coverImage');
+const houseImageRoutes = require('./api/routes/user/houseImage');
+const adminRoutes = require('./api/routes/admin/admin');
+const divisionRoutes = require('./api/routes/admin/division');
+const locationRoutes = require('./api/routes/admin/location');
+const maxAreaRoutes = require('./api/routes/admin/maxArea');
+const maxMoneyRoutes = require('./api/routes/admin/maxMoney');
+const minAreaRoutes = require('./api/routes/admin/minArea');
+const minMoneyRoutes = require('./api/routes/admin/minMoney');
+const roomRoutes = require('./api/routes/admin/room');
 
-mongoose.connect('mongodb://localhost:27017/PersonalBlog', {
+mongoose.connect('mongodb://localhost:27017/HouseRenting', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
@@ -15,6 +27,21 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/user', userRoutes);
+app.use('/houseads', houseAdsRoutes);
+app.use('/coverimg', coverImageRoutes);
+app.use('/houseimg', houseImageRoutes);
+app.use('/admin', adminRoutes);
+app.use('/division', divisionRoutes);
+app.use('/location', locationRoutes);
+app.use('/maxarea', maxAreaRoutes);
+app.use('/maxmoney', maxMoneyRoutes);
+app.use('/minarea', minAreaRoutes);
+app.use('/minmoney', minMoneyRoutes);
+app.use('/room', roomRoutes);
+
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     const error = new Error("Not found");
