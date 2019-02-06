25f57f898fd37a30b7c7f43ae6a01f268b0121c9
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-06 17:37:43 +0600

    maxArea route is updated

--- api/routes/admin/maxArea.js
+++ api/routes/admin/maxArea.js
@@ -4,8 +4,9 @@ const checkAdminAuth = require('../../middleware/adminAuth');
 const maxAreaController = require('../../controller/admin/maxArea');
 
 router.get('/all', maxAreaController.getAllMaxArea);
+router.get('/:maxAreaID', maxAreaController.getOneMaxArea);
 router.post('/create', maxAreaController.createMaxArea);
 router.patch('/update/:maxAreaID', maxAreaController.updateMaxArea);
-router.delete('/delete/:maxAreaID', maxAreaController.deteleteMaxArea);
+router.delete('/delete/:maxAreaID', maxAreaController.deleteMaxArea);
 
 module.exports = router;
