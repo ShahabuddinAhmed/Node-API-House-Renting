324272702807e86f56c1d487adfa41c90527b750
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-16 00:43:53 +0600

    multiple image uploder is added

--- api/routes/user/houseImage.js
+++ api/routes/user/houseImage.js
@@ -8,17 +8,18 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, '../../../public/image');
+        cb(null, './public/image');
     },
     filename: function(req, file, cb) {
-        cb(null, dateFormat('yyyymmddHHMMss').toString() + path.extname(file.originalname));
+        // cb(null, dateFormat('yyyymmddHHMMss').toString() + path.extname(file.originalname));
+        cb(null, dateFormat('yyyymmddHHMMss').toString() + '-' + file.originalname);
     }
 });
 
 const upload = multer({ storage: storage });
 
-router.get('/:houseImageID', houseImageController.getOneHouseImage);
-router.post('/create', upload.single('image'), houseImageController.createHouseImage);
-router.delete('/delete/:houseImageID', houseImageController.deleteHouseImage);
+router.get('/:houseAdsID', houseImageController.getOneHouseImage);
+router.post('/create', upload.single('small'), houseImageController.createHouseImage);
+router.delete('/delete/:houseAdsID', houseImageController.deleteHouseImage);
 
 module.exports = router;
