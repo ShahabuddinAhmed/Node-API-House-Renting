b319d190bb3cfdd4d62cf2907d9940125666f792
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-26 22:35:47 +0600

    user routes is updated

--- api/routes/user/user.js
+++ api/routes/user/user.js
@@ -6,8 +6,8 @@ const userController = require('../../controller/user/user');
 router.get('/all', userController.getAllUser);
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/:userID', userController.getOneUser);
-router.patch('/update/:userID', userController.updateUser);
-router.delete('/delete/:userID', userController.deleteUser);
+router.get('/:userID', userController.getOneUser, checkUserAuth);
+router.patch('/update/:userID', userController.updateUser, checkUserAuth);
+router.delete('/delete/:userID', userController.deleteUser, checkUserAuth);
 
 module.exports = router;
